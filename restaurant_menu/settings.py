2ff83b49700a52488cf71f25ba300c5e37a63f4f
Suraj Rajput <surajpatil108>
2026-05-16 13:57:23 +0530

    fix for storage

--- restaurant_menu/settings.py
+++ restaurant_menu/settings.py
@@ -86,7 +86,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
 
